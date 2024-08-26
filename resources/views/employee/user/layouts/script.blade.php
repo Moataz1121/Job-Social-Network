bbdcf1ed1a67de1f25d333d74b14c3a2ac803063
“hamo4651” <hamo4651@gmail.com>
2024-08-26 14:31:26 +0300

    secon1d

---
+++ resources/views/employee/user/layouts/script.blade.php
@@ -0,0 +1,16 @@
+
+    <!-- Core -->
+    <script src="front/assets/js/jquery/jquery-3.3.1.min.js"></script>
+    <script src="front/assets/js/popper/popper.min.js"></script>
+    <script src="front/assets/js/bootstrap/bootstrap.min.js"></script>
+    <!-- Optional -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script type="text/javascript">
+        $("#menu-toggle").click(function(e) {
+            e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
+        });
+
+    </script>
+    <script src="front/assets/js/app.js"></script>
+    <script src="front/assets/js/components/components.js"></script>
