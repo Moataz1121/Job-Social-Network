00bd8bf0887014e87bb26b104383a71d8b013701
unknown <af348565@gmail.com>
2024-08-26 19:31:15 +0300

    Secoend Migrate

---
+++ database/migrations/2024_08_26_162608_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->String('title');
+            $table->text('description');
+            $table->String('location');
+            $table->enum ('work_type', ['onSite', 'Hybrid', 'Remote']);
+            $table->integer('salary');
+            $table->date('deadLine');
+            $table->text('skills');
+            $table->enum('status', ['pending', 'accepted', 'rejected']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
