50e99394bf7776fba08449072c699155efabb117
unknown <af348565@gmail.com>
2024-09-07 11:13:50 +0300

    send e-mail for user from employer

--- app/Models/PostsEmployee.php
+++ app/Models/PostsEmployee.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class PostsEmployee extends Model
 {
     use HasFactory;
+    use Notifiable;
     protected $fillable = [
         'user_id', 
         'post_id',
@@ -16,7 +18,7 @@ class PostsEmployee extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id', 'id');
     }
 
     public function post()
